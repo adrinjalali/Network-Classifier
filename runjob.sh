3db4bb0f699247bb8941fc181f73d8e8d9c4b53d
adrin <adrin.jalali@gmail.com>
2015-08-27 19:19:47 +0200

    ...

--- runjob.sh
+++ runjob.sh
@@ -4,4 +4,4 @@ env
 date
 
 #python3.3 run.py --input-dir $1 --working-dir $2 --method $3 --cv-index $SGE_TASK_ID --batch-based
-ipython3 -i run.py -- --input-dir $1 --working-dir $2 --method $3 --cv-index $SGE_TASK_ID --batch-based --cpu-count 10
+ipython3 -i run.py -- --input-dir $1 --working-dir $2 --method $3 --cv-index $SGE_TASK_ID --batch-based --cpu-count 50
